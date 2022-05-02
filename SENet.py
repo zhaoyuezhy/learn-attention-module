7b493dae1e9152d0361e4f810f2baf75c04e3209
zhaoyuezhy <229074040@qq.com>
2022-05-02 17:11:18 +0800

    main

---
+++ SENet.py
@@ -0,0 +1,82 @@
+### Keras实现的SENet moudle
+### 网上找的代码
+#
+
+
+# GRADED FUNCTION: ResNet50
+ 
+def ResNet50(input_shape = (64, 64, 3), classes = 6):
+    """
+    Implementation of the popular ResNet50 the following architecture:
+    CONV2D -> BATCHNORM -> RELU -> MAXPOOL -> CONVBLOCK -> IDBLOCK*2 -> CONVBLOCK -> IDBLOCK*3
+    -> CONVBLOCK -> IDBLOCK*5 -> CONVBLOCK -> IDBLOCK*2 -> AVGPOOL -> TOPLAYER
+    Arguments:
+    input_shape -- shape of the images of the dataset
+    classes -- integer, number of classes
+    Returns:
+    model -- a Model() instance in Keras
+    """
+    
+    # Define the input as a tensor with shape input_shape
+    X_input = Input(input_shape)
+ 
+    
+    # Zero-Padding
+    X = ZeroPadding2D((3, 3))(X_input)
+    
+    # Stage 1
+    X = Conv2D(filters=64, kernel_size=(7, 7), strides=(2, 2), name="conv",
+               kernel_initializer=glorot_uniform(seed=0))(X)
+    X = BatchNormalization(axis=3, name="bn_conv1")(X)
+    X = Activation("relu")(X)
+    X = MaxPooling2D(pool_size=(3, 3), strides=(2, 2))(X)
+ 
+    # Stage 2
+    X = convolutional_block(X, f=3, filters=[64, 64, 256], stage=2, block="a", s=1)
+    X = identity_block(X, f=3, filters=[64, 64, 256], stage=2, block="b")
+    X = identity_block(X, f=3, filters=[64, 64, 256], stage=2, block="c")
+    ### START CODE HERE ###
+ 
+    # Stage 3 (≈4 lines)
+    # The convolutional block uses three set of filters of size [128,128,512], "f" is 3, "s" is 2 and the block is "a".
+    # The 3 identity blocks use three set of filters of size [128,128,512], "f" is 3 and the blocks are "b", "c" and "d".
+    X = convolutional_block(X, f=3, filters=[128, 128, 512], stage=3, block="a", s=1)
+    X = identity_block(X, f=3, filters=[128, 128, 512], stage=3, block="b")
+    X = identity_block(X, f=3, filters=[128, 128, 512], stage=3, block="c")
+    X = identity_block(X, f=3, filters=[128, 128, 512], stage=3, block="d")
+    
+    # Stage 4 (≈6 lines)
+    # The convolutional block uses three set of filters of size [256, 256, 1024], "f" is 3, "s" is 2 and the block is "a".
+    # The 5 identity blocks use three set of filters of size [256, 256, 1024], "f" is 3 and the blocks are "b", "c", "d", "e" and "f".
+    X = convolutional_block(X, f=3, filters=[256, 256, 1024], stage=4, block="a", s=2)
+    X = identity_block(X, f=3, filters=[256, 256, 1024], stage=4, block="b")
+    X = identity_block(X, f=3, filters=[256, 256, 1024], stage=4, block="c")
+    X = identity_block(X, f=3, filters=[256, 256, 1024], stage=4, block="d")
+    X = identity_block(X, f=3, filters=[256, 256, 1024], stage=4, block="e")
+    X = identity_block(X, f=3, filters=[256, 256, 1024], stage=4, block="f")
+    
+ 
+    # Stage 5 (≈3 lines)
+    # The convolutional block uses three set of filters of size [512, 512, 2048], "f" is 3, "s" is 2 and the block is "a".
+    # The 2 identity blocks use three set of filters of size [256, 256, 2048], "f" is 3 and the blocks are "b" and "c".
+    X = convolutional_block(X, f=3, filters=[512, 512, 2048], stage=5, block="a", s=2)
+    X = identity_block(X, f=3, filters=[512, 512, 2048], stage=5, block="b")
+    X = identity_block(X, f=3, filters=[512, 512, 2048], stage=5, block="c")
+    
+    # filters should be [256, 256, 2048], but it fail to be graded. Use [512, 512, 2048] to pass the grading
+    
+ 
+    # AVGPOOL (≈1 line). Use "X = AveragePooling2D(...)(X)"
+    # The 2D Average Pooling uses a window of shape (2,2) and its name is "avg_pool".
+    X = AveragePooling2D(pool_size=(2, 2), padding="same")(X)
+    
+    ### END CODE HERE ###
+ 
+    # output layer
+    X = Flatten()(X)
+    X = Dense(classes, activation="softmax", name="fc"+str(classes), kernel_initializer=glorot_uniform(seed=0))(X)
+    
+    # Create model
+    model = Model(inputs=X_input, outputs=X, name="ResNet50")
+ 
+    return model
